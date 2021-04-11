213752453b7f20f464da4ede6b124f9a3ddaf0ea
ramziddin_olimov <ramizmafiya@gmail.com>
2021-04-11 14:03:37 +0500

    modal started

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//menu button
+
 let menuBtnElement = document.querySelector('.hamburger-btn')
 let menuElement = document.querySelector('.menu')
 let shadowElement = document.querySelector('.shadow')
@@ -14,4 +16,22 @@ shadowElement.addEventListener ('click', () => {
     shadowElement.classList.add('shadow-hidden')
     menuElement.style.transform = 'translateX(-100%)'
 })
+
+// Setting button
+
+let settingBtnElement = document.querySelector('.setting-btn')
+let settingElement = document.querySelector('.settings')
+
+settingBtnElement.addEventListener ('click', () => {
+    settingElement.classList.remove('modal-settings')
+    shadowElement.classList.remove('shadow-hidden')
+    settingElement.style.transform = 'translate(0%)'
+})
+
+shadowElement.addEventListener ('click', () => {
+    settingElement.classList.add('modal-settings')
+    shadowElement.classList.add('shadow-hidden')
+    settingElement.style.transform = 'translate(100%)'
+})
+
 
